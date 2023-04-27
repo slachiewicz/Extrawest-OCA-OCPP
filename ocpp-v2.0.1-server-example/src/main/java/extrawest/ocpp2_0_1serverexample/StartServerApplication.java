d4e976d740b56c7d8a89b4b8feba2ffe4c05f3fb
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-04-28 02:16:04 +0300

    added new module ocpp-v2.0.1-server-example

---
+++ ocpp-v2.0.1-server-example/src/main/java/extrawest/ocpp2_0_1serverexample/StartServerApplication.java
@@ -0,0 +1,13 @@
+package extrawest.ocpp2_0_1serverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartServerApplication.class, args);
+    }
+
+}
